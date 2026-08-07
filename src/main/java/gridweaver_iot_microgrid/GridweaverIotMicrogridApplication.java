6e8d4a0a9a8227a4f45c526223805c7be7ae58a0
Sameer Vhatkar <exploringcodes@gmail.com>
2026-08-07 23:54:28 +0530

    feat: initialize Spring Boot 3 project with Java 21 virtual threads

---
+++ src/main/java/gridweaver_iot_microgrid/GridweaverIotMicrogridApplication.java
@@ -0,0 +1,13 @@
+package gridweaver_iot_microgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridweaverIotMicrogridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridweaverIotMicrogridApplication.class, args);
+	}
+
+}
