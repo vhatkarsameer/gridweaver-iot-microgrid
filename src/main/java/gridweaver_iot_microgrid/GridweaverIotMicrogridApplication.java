8f207324b17b2a2aa7e99c4636ef2f0dacd6b006
Sameer Vhatkar <exploringcodes@gmail.com>
2026-08-08 23:20:26 +0530

    feat: complete Java Virtual Thread IoT telemetry simulator & websocket server

--- src/main/java/gridweaver_iot_microgrid/GridweaverIotMicrogridApplication.java
+++ src/main/java/gridweaver_iot_microgrid/GridweaverIotMicrogridApplication.java
@@ -2,8 +2,10 @@ package gridweaver_iot_microgrid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GridweaverIotMicrogridApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/gridweaver_iot_microgrid/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package gridweaver_iot_microgrid.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-grid")
+                .setAllowedOriginPatterns("*");
+    }
+}
