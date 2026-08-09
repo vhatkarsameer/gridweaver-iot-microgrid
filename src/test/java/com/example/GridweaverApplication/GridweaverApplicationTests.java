93df6f2641b9cc07363646a8652a0b49538633ea
venkatachalapathy08 <venkat221885@gmail.com>
2026-08-09 19:33:47 +0530

    creating a project structure

---
+++ src/test/java/com/example/GridweaverApplication/GridweaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GridweaverApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridweaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
