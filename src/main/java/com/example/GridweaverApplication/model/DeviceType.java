09841647dad0d480bb81f30c88019c54669c85ba
Sameer Vhatkar <exploringcodes@gmail.com>
2026-08-14 15:45:32 +0530

    Revert "Feature/week 1 backend documentation and testing"

--- src/main/java/com/example/GridweaverApplication/model/DeviceType.java
+++
@@ -1,6 +0,0 @@
-package com.example.GridweaverApplication.model;
-
-public enum DeviceType {
-    SOLAR_PANEL,
-    BATTERY
-}
--- src/main/java/com/example/GridweaverApplication/model/HouseholdLocation.java
+++
@@ -1,7 +0,0 @@
-package com.example.GridweaverApplication.model;
-
-public record HouseholdLocation(
-        String houseId,
-        double latitude,
-        double longitude
-) {}
